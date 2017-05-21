49ad8c795cf1fcb4e560707be173ba4a6a97a854
森太郎 <notion9999@yahoo.co.jp>
2017-05-21 18:03:01 +0900

    Add start button

---
+++ app/src/main/java/com/morimori/toyamabenquizu/MainActivity.java
@@ -0,0 +1,25 @@
+package com.morimori.toyamabenquizu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		((Button)findViewById(R.id.startButton)).setOnClickListener(this);
+	}
+
+
+	@Override
+	public void onClick(View v)
+	{
+
+	}
+}
